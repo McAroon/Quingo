387b7090bd1b01f126a92c8c66d1fa28d1d0de6d
McAroon <pgrabel@gmail.com>
2024-10-03 14:10:02 +0300

    allow external image urls

--- Quingo/Infrastructure/Files/FileStoreService.cs
+++ Quingo/Infrastructure/Files/FileStoreService.cs
@@ -2,10 +2,11 @@
 using Amazon.S3.Model;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.Extensions.Options;
+using System.Text.RegularExpressions;
 
 namespace Quingo.Infrastructure.Files;
 
-public class FileStoreService
+public partial class FileStoreService
 {
     private readonly FileStoreSettings _fileSettings;
     private readonly IAmazonS3 _client;
@@ -15,6 +16,11 @@ public class FileStoreService
         _fileSettings = storageOptions.Value;
         _client = client;
     }
+
+    [GeneratedRegex("^https{0,1}:\\/\\/")]
+    private static partial Regex UrlRegex();
+
+    private readonly Regex _urlRegex = UrlRegex();
 
     public async Task<string> UploadBrowserFile(IBrowserFile file)
     {
@@ -35,6 +41,8 @@ public class FileStoreService
 
     public string GetFileUrl(string? filename)
     {
+        if (filename != null && _urlRegex.IsMatch(filename)) return filename;
+
         var prefix = _fileSettings.UrlPrefix.EndsWith('/') ? _fileSettings.UrlPrefix : _fileSettings.UrlPrefix + "/";
         return prefix + filename;
     }
