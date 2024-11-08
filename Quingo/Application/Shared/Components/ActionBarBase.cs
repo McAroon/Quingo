4cdff6a0f2d9e2a58afb4895e89f73a5111774e2
McAroon <pgrabel@gmail.com>
2024-11-08 18:10:01 +0200

    added host controls for player screen

---
+++ Quingo/Application/Shared/Components/ActionBarBase.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Quingo.Application.State;
+
+namespace Quingo.Application.Shared.Components;
+
+public abstract class ActionBarBase : ComponentBase
+{
+    [Inject]
+    protected ILogger<ActionBarBase> Logger { get; set; } = default!;
+    
+    [Inject]
+    protected GameStateService StateService { get; set; } = default!;
+    
+    [Inject]
+    protected IDialogService DialogService { get; set; } = default!;
+    
+    [Inject]
+    protected ISnackbar Snackbar { get; set; } = default!;
+    
+    public virtual GameState Game { get; set; } = default!;
+    public virtual string UserId { get; set; } = default!;
+    
+    
+    protected async Task ConfirmEndGame()
+    {
+        var parameters = new ConfirmDialog.ConfirmDialogParams
+        {
+            Prompt = $"Do you want to end game?",
+            ButtonText = "End game",
+            ButtonColor = Color.Error
+        };
+        var confirmed = await ConfirmDialog.CallDialog(DialogService, parameters);
+
+        if (confirmed)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        try
+        {
+            StateService.EndGame(Game.GameSessionId, UserId);
+        }
+        catch (Exception e)
+        {
+            Logger.LogError(e, e.Message);
+            Snackbar.Add(e.Message, Severity.Error);
+        }
+    }
+
+    protected void Draw()
+    {
+        try
+        {
+            Game.Draw();
+        }
+        catch (Exception e)
+        {
+            Logger.LogError(e, e.Message);
+            Snackbar.Add(e.Message, Severity.Error);
+        }
+    }
+}
--- Quingo/wwwroot/interop.js
+++ Quingo/wwwroot/interop.js
@@ -1,10 +1,18 @@
 window.scrollToElementId = (elementId) => {
     const element = document.getElementById(elementId);
-    if(!element)
-    {
+    if(!element) {
         return false;
     }
     
     window.scroll({ top: element.offsetTop, behavior: 'smooth' });
     return true;
+};
+
+window.scrollToLastChild = (elementId) => {
+  const element = document.getElementById(elementId);
+  if (!element) {
+      return false;
+  }
+  element.lastElementChild.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'center'});
+  return true;
 };
